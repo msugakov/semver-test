0a20b988d13e401a77cf8342a978e7c78d6d71c8
Kyle Lape <klape@redhat.com>
2023-11-14 12:58:22 -0600

    Add in semver dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kylape/semver-test
 
 go 1.20
+
+require github.com/blang/semver/v4 v4.0.0
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	semver "github.com/blang/semver/v4"
+)
+
 func main() {
-	print("Hello, world!")
+	println("Hello, world!")
+	v1, err := semver.Make("1.0.0-beta")
+	v2, err := semver.Make("2.0.0-beta")
+
+	if err != nil {
+		panic(err)
+	}
+
+	println(v1.Compare(v2))
 }
